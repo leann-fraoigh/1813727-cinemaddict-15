964398f9b67c3aecdbe4cf7b5a226ce0a022883b
Leann Fraoigh <d.myshlennikova@gmail.com>
2021-08-22 13:38:56 +0300

    Добавляет наследование простым компонентам

--- src/view/more-button.js
+++ src/view/more-button.js
@@ -1,28 +1,12 @@
-import {createElement} from '../utils.js';
+import AbstractView from './abstract.js';
 
 const createMoreBtnTemplate = () => (
   '<button class="films-list__show-more">Show more</button>'
 );
 
 
-export default class MoreBtn {
-  constructor() {
-    this._element = null;
-  }
-
+export default class MoreBtn extends AbstractView {
   getTemplate() {
     return createMoreBtnTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
